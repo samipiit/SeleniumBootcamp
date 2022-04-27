27b1456700651cfd8508ececb2fdb33955f62a7b
samipiit <sami@piit.us>
2022-04-26 22:50:49 -0400

    Fixed modules to build mvn jars successfully

---
+++ com.att/src/test/java/testPackage/TestClass.java
@@ -0,0 +1,11 @@
+package testPackage;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+    @Test
+    public void testTesterMethod() {
+        System.out.println("Success !");
+    }
+}
